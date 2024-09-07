7e3182157f1b70bf60834b7703aabecd5a6c34af
CoCoKayya18 <collins1999.ck@gmail.com>
2024-09-07 21:43:12 +0200

    Added ML model and currently implementing prediction step. Visualization is not working yet.

--- src/ekf_slam_pkg/include/ekf_slam_pkg/ekf.py
+++ src/ekf_slam_pkg/include/ekf_slam_pkg/ekf.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pickle
 import rospy
+from geometry_msgs.msg import Pose
+from tf.transformations import euler_from_quaternion
+from tf.transformations import quaternion_from_euler
+
 
 class EKFSLAM:
     def __init__(self, robot, sensor, map, config, utils):
@@ -9,29 +13,66 @@ class EKFSLAM:
         self.map = map
         self.config = config
         self.utils = utils
-        self.state = np.zeros((3, 1))  # Example: [x, y, theta]
+        self.state = Pose()
         self.process_noise = config['process_noise']
         self.measurement_noise = config['measurement_noise']
+        self.predictedPose = Pose()
+
+        model_path = "../Spezialisierung-2/src/ekf_slam_pkg/myMLModel/sparse_gpy_model_random3_odometry.pkl"
+        scalerX_path = "../Spezialisierung-2/src/ekf_slam_pkg/Scaler/sparse_scaler_X_random3_odometry.pkl"
+        scalerY_path = "../Spezialisierung-2/src/ekf_slam_pkg/Scaler/sparse_scaler_Y_random3_odometry.pkl"
+        
+        # Load the scalers
+        with open(scalerX_path, 'rb') as file:
+            self.scaler_X = pickle.load(file)
+        with open(scalerY_path, 'rb') as file:
+            self.scaler_Y = pickle.load(file)
+
+        with open(model_path, 'rb') as file:
+            self.model = pickle.load(file)
+
         rospy.loginfo("EKF Class initialized")
         
 
     # EKF prediction step
-    def predict(self, currentPose):
+    def predict(self, currentVel, currentPosition):
 
-        print("It works:" + str(currentPose))
+        currentVel = np.array(currentVel).reshape(1, -1)  # Ensure it has the shape (1, 3)
 
-        # loaded_model = pickle.load(filepath)
+        # Scale the input data correctly
+        odomVelScaled = self.scaler_X.transform(currentVel)
 
-        # # Predict using the loaded model and the standardized data
-        # y_predict_mean, y_predict_variance = loaded_model.predict()
+        # Predict using the loaded model and the standardized data
+        y_predict_mean, y_predict_variance = self.model.predict(odomVelScaled)
 
-        # # Print the model parameters
-        # print(loaded_model)
-        self.predictedPose = currentPose
-        return self.predictedPose
+        predictedDelta = self.scaler_Y.inverse_transform(y_predict_mean)
+
+        orientation_q = currentPosition.orientation
+        orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+        (roll, pitch, theta) = euler_from_quaternion(orientation_list)
+
+        currentPosition.position.x += predictedDelta[0][0]  # Add the first delta to x
+        currentPosition.position.y += predictedDelta[0][1]  # Add the second delta to y
+        currentPosition.position.z = 0
+        theta += predictedDelta[0][2]  # Add the third delta to theta
+
+        # Convert updated theta back to quaternion
+        q = quaternion_from_euler(0, 0, theta)
+
+        # Set the orientation using the updated quaternion
+        currentPosition.orientation.x = q[0]
+        currentPosition.orientation.y = q[1]
+        currentPosition.orientation.z = q[2]
+        currentPosition.orientation.w = q[3]
+
+        # Update state
+        self.state = currentPosition
+
+        return self.state
 
     def correct(self, scanMessage):
-        print("It works too:" + str(scanMessage))
+        # print("It works too:" + str(scanMessage))
         # EKF update step
-        self.correctedPose = self.predictedPose
-        return self.correctedPose
+        correctedPose = None
+        self.state = correctedPose
+        return self.state
--- src/ekf_slam_pkg/include/ekf_slam_pkg/robot.py
+++ src/ekf_slam_pkg/include/ekf_slam_pkg/robot.py
@@ -1,12 +1,28 @@
 import rospy
+import numpy as np
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Point
+from geometry_msgs.msg import Pose
+from tf.transformations import quaternion_from_euler
 
 class Robot:
     def __init__(self, config, ekf_slam):
-        self.position = config['initial_position']
+
+        #self.position = config['initial_position']
+        self.position = Pose()
+        self.position.position.x = config['initial_position']['x']
+        self.position.position.y = config['initial_position']['y']
+        self.position.position.z = 0  # Assuming the robot is in 2D space
+
+        # Initialize orientation using Euler angles to quaternion (assuming yaw in config)
+        quaternion = quaternion_from_euler(0, 0, config['initial_position']['theta'])
+        self.position.orientation.x = quaternion[0]
+        self.position.orientation.y = quaternion[1]
+        self.position.orientation.z = quaternion[2]
+        self.position.orientation.w = quaternion[3]
+
         self.ekf_slam = ekf_slam
         rospy.loginfo("Robot class initialized")
 
@@ -19,20 +35,31 @@ class Robot:
         self.EKF_path_pub = rospy.Publisher('/EKF_Path', Marker, queue_size=10)
         
         self.current_pose = None
+        self.current_vel = np.zeros((3, 1))
         self.ground_truth_path = []
         self.ekf_path = []
         
     def odom_callback(self, msg):
-        self.current_pose = msg.pose.pose
-        ekf_predicted_pose = self.ekf_slam.predict(self.current_pose)  # Run the EKF prediction
+        self.current_pose = self.position
+        self.current_vel[0] = msg.twist.twist.linear.x
+        self.current_vel[1] = msg.twist.twist.linear.y
+        self.current_vel[2] = msg.twist.twist.angular.z
+        ekf_predicted_pose = self.ekf_slam.predict(self.current_vel, self.current_pose)  # Run the EKF prediction
+        
+        self.position = ekf_predicted_pose
+        
         self.ekf_path.append(ekf_predicted_pose)
         self.publish_EKF_path(self.ekf_path, "ekf_path", [0.0, 0.0, 1.0])  # Blue path
 
     def scan_callback(self, msg):
         self.scan_message = msg
-        ekf_corrected_pose = self.ekf_slam.correct(self.scan_message)
-        self.ekf_path.append(ekf_corrected_pose)
-        self.publish_EKF_path(self.ekf_path, "ekf_path", [0.0, 0.0, 1.0])  # Blue path
+        
+        # ekf_corrected_pose = self.ekf_slam.correct(self.scan_message)
+
+        # self.position = ekf_corrected_pose
+        
+        # self.ekf_path.append(ekf_corrected_pose)
+        # self.publish_EKF_path(self.ekf_path, "ekf_path", [1.0, 0.0, 0.0])  # Red path
 
     def ground_truth_callback(self, msg):
         self.ground_truth_path.append(msg.pose.pose)
@@ -51,6 +78,7 @@ class Robot:
         marker.color.g = color[1]
         marker.color.b = color[2]
         marker.color.a = 0.5
+        marker.lifetime = rospy.Duration(0)
 
         marker.points = [Point(p.position.x, p.position.y, p.position.z) for p in path]
 
@@ -64,13 +92,21 @@ class Robot:
         marker.id = 0
         marker.type = Marker.LINE_STRIP
         marker.action = Marker.ADD
-        marker.scale.x = 0.03  # Line width
+        marker.scale.x = 3  # Line width
         marker.color.r = color[0]
         marker.color.g = color[1]
         marker.color.b = color[2]
-        marker.color.a = 0.5
+        marker.color.a = 1
+        # Set the default pose for the marker (identity pose)
+        marker.pose.orientation.w = 1.0
+        marker.lifetime = rospy.Duration(0)
 
         marker.points = [Point(p.position.x, p.position.y, p.position.z) for p in path]
+
+        # print("Point: ")
+        # print(marker.points)
+
+        print(f"Number of points in marker: {len(marker.points)}")
 
         self.EKF_path_pub.publish(marker)
     
